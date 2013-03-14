b8d5a25f31590792a35a8396aa0af5fb8af91fd4
ShaB <shab@ubuntu.(none)>
2013-03-14 18:53:47 +0200

    airbreak with wrong key

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,4 @@
 Airbrake.configure do |config|
   #config.api_key = '0363bd0847b94b9d07d96b02b07b25f0'
-  config.api_key = '3b497375a196aeffbc7de0f793b88f20'
+  config.api_key = '3b497375a196aeffbc7de0f793b88f2'
 end
